66a488bc38f57b24ba38196517814d4deb5261cf
陈sir <13837629826@163.com>
2022-01-07 23:59:32 +0800

    leetcode

---
+++ Js高频题/leetcode/editor/cn/[15]三数之和.js
@@ -0,0 +1,46 @@
+//<p>给你一个包含 <code>n</code> 个整数的数组 <code>nums</code>，判断 <code>nums</code> 中是否存在三个元素 <em>a，b，c ，</em>使得 <em>a + b + c = </em>0 ？请你找出所有和为 <code>0</code> 且不重复的三元组。</p>
+//
+//<p><strong>注意：</strong>答案中不可以包含重复的三元组。</p>
+//
+//<p> </p>
+//
+//<p><strong>示例 1：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>nums = [-1,0,1,2,-1,-4]
+//<strong>输出：</strong>[[-1,-1,2],[-1,0,1]]
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>nums = []
+//<strong>输出：</strong>[]
+//</pre>
+//
+//<p><strong>示例 3：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>nums = [0]
+//<strong>输出：</strong>[]
+//</pre>
+//
+//<p> </p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul>
+//	<li><code>0 <= nums.length <= 3000</code></li>
+//	<li><code>-10<sup>5</sup> <= nums[i] <= 10<sup>5</sup></code></li>
+//</ul>
+//<div><div>Related Topics</div><div><li>数组</li><li>双指针</li><li>排序</li></div></div><br><div><li>👍 4165</li><li>👎 0</li></div>
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
