8471c0089e84f64a9f6b8814b56c0644e1e9b7b5
陈sir <13837629826@163.com>
2022-02-06 23:56:52 +0800

    leetcode

---
+++ Js高频题/leetcode/editor/cn/[84]柱状图中最大的矩形.js
@@ -0,0 +1,45 @@
+//<p>给定 <em>n</em> 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。</p>
+//
+//<p>求在该柱状图中，能够勾勒出来的矩形的最大面积。</p>
+//
+//<p> </p>
+//
+//<p><strong>示例 1:</strong></p>
+//
+//<p><img src="https://assets.leetcode.com/uploads/2021/01/04/histogram.jpg" /></p>
+//
+//<pre>
+//<strong>输入：</strong>heights = [2,1,5,6,2,3]
+//<strong>输出：</strong>10
+//<strong>解释：</strong>最大的矩形为图中红色区域，面积为 10
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<p><img src="https://assets.leetcode.com/uploads/2021/01/04/histogram-1.jpg" /></p>
+//
+//<pre>
+//<strong>输入：</strong> heights = [2,4]
+//<b>输出：</b> 4</pre>
+//
+//<p> </p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul>
+//	<li><code>1 <= heights.length <=10<sup>5</sup></code></li>
+//	<li><code>0 <= heights[i] <= 10<sup>4</sup></code></li>
+//</ul>
+//<div><div>Related Topics</div><div><li>栈</li><li>数组</li><li>单调栈</li></div></div><br><div><li>👍 1739</li><li>👎 0</li></div>
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+
+
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
