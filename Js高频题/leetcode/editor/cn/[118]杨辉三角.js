da003c22be991420d16f160a45a55ad74eec4236
陈sir <13837629826@163.com>
2022-03-10 14:16:56 +0800

    le

---
+++ Js高频题/leetcode/editor/cn/[118]杨辉三角.js
@@ -0,0 +1,40 @@
+//<p>给定一个非负整数 <em><code>numRows</code>，</em>生成「杨辉三角」的前 <em><code>numRows</code> </em>行。</p>
+//
+//<p><small>在「杨辉三角」中，每个数是它左上方和右上方的数的和。</small></p>
+//
+//<p><img alt="" src="https://pic.leetcode-cn.com/1626927345-DZmfxB-PascalTriangleAnimated2.gif" /></p>
+//
+//<p> </p>
+//
+//<p><strong>示例 1:</strong></p>
+//
+//<pre>
+//<strong>输入:</strong> numRows = 5
+//<strong>输出:</strong> [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//</pre>
+//
+//<p><strong>示例 2:</strong></p>
+//
+//<pre>
+//<strong>输入:</strong> numRows = 1
+//<strong>输出:</strong> [[1]]
+//</pre>
+//
+//<p> </p>
+//
+//<p><strong>提示:</strong></p>
+//
+//<ul>
+//	<li><code>1 <= numRows <= 30</code></li>
+//</ul>
+//<div><div>Related Topics</div><div><li>数组</li><li>动态规划</li></div></div><br><div><li>👍 703</li><li>👎 0</li></div>
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
